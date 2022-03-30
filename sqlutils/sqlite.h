a4af83528f1f244e1c26cc2aadb2997a053a1fdd
TonyBeen <1772747354@qq.com>
2022-03-30 09:17:37 +0800

    update

---
+++ sqlutils/sqlite.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: sqlite.h
+    > Author: hsz
+    > Brief:
+    > Created Time: 2022-03-30 09:16:13 Wednesday
+ ************************************************************************/
+
+#ifndef __EULAR_SQLITE_H__
+#define __EULAR_SQLITE_H__
+
+#include <sqlite3.h>
+
+namespace eular {
+
+} // namespace eular
+
+#endif // __EULAR_SQLITE_H__
