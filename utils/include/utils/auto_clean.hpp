1c4b075986ead219533fc799fa2067dc681087ac
TonyBeen <1772747354@qq.com>
2024-07-01 15:00:48 +0800

    update

---
+++ utils/include/utils/auto_clean.hpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+    > File Name: auto_clean.hpp
+    > Author: hsz
+    > Brief:
+    > Created Time: 2024年07月01日 星期一 14时38分07秒
+ ************************************************************************/
+
+#ifndef __EULAR_UTILS_AUTO_CLEAN_H__
+#define __EULAR_UTILS_AUTO_CLEAN_H__
+
+#include <functional>
+
+namespace eular {
+
+class AutoClean final
+{
+    using Callback = std::function<void(void)>;
+
+public:
+    AutoClean(Callback cleanCb) : m_cleanCb(cleanCb) { }
+    ~AutoClean()
+    {
+        if (m_cleanCb)
+        {
+            m_cleanCb();
+        }
+    }
+
+private:
+    Callback m_cleanCb;
+};
+
+} // namespace eular
+
+#endif // __EULAR_UTILS_AUTO_CLEAN_H__
